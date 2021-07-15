6ac8f9625951bd031700c3945e91f02227f44e0d
Andrey Oleynik <andrei.oleynik@gmail.com>
2021-07-16 00:03:33 +0200

    Updated hamcrest assertions to a separate dependency. Added property variables to pom xml

--- src/test/java/com/dataart/qa/workshop/test/JUnitParamsTest.java
+++ src/test/java/com/dataart/qa/workshop/test/JUnitParamsTest.java
@@ -7,12 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import junitparams.Parameters;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @RunWith(JUnitParamsRunner.class)
 public class JUnitParamsTest {
     public static Object[][] numbers() {
-        return new Object[][] {
+        return new Object[][]{
                 {1, 1},
                 {5, 120},
         };
@@ -20,7 +21,7 @@ public class JUnitParamsTest {
 
     @Test
     @Parameters(method = "numbers")
-    public void test_factorial_with_junitparams(long number, long expected) {
+    public void test_factorial_with_junit_params(long number, long expected) {
         Assert.assertEquals("Factorial function is wrong.", expected, Factorial.factorial(number));
     }
 
@@ -30,13 +31,13 @@ public class JUnitParamsTest {
             "2,2,4",
             "2,2,4"//5
     })
-    public void test_multiplication_with_junitparams(int x, int y, int z) {
-        Assert.assertThat(x * y, is(z));
+    public void test_multiplication_with_junit_params(int x, int y, int z) {
+        assertThat(x * y, is(z));
         System.out.println("test2");
     }
 
     @Test
-    public void non_parametrised_test(){
+    public void non_parametrised_test() {
         System.out.println("Something here.");
     }
 }
