5e9d08f9ace0296fb98fffe3f17861bd733fd4b1
Andrey Oleynik <5436433+a-oleynik@users.noreply.github.com>
2021-07-24 19:33:59 +0200

    updated the db resource test

--- src/test/java/com/oleynik/qa/workshop/junit/rules/DBResourceExtensionTest.java
+++ src/test/java/com/oleynik/qa/workshop/junit/rules/DBResourceExtensionTest.java
@@ -1,34 +1,34 @@
 package com.oleynik.qa.workshop.junit.rules;
 
-import org.junit.Assert;
-import org.junit.Rule;
+import com.oleynik.qa.workshop.junit.extension.DBResourceExtension;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExternalResource;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.runner.OrderWith;
 import org.junit.runner.manipulation.Alphanumeric;
 
 @OrderWith(Alphanumeric.class)
-public class ExternalResourceRuleTest {
-    @Rule
-    public ExternalResource rule = new DBResourceRule();
+public class DBResourceExtensionTest {
+    @RegisterExtension
+    static final DBResourceExtension MY_RESOURCE = new DBResourceExtension();
 
     @Test
     public void external_resource_rule_multiplication_test() {
         System.out.println("Multiplication test");
-        Assert.assertEquals("Multiplication result is wrong", 10, 2 * 5);
+        Assertions.assertEquals(10, 2 * 5, "Multiplication result is wrong");
     }
 
     @Test
     public void external_resource_rule_division_test() {
         System.out.println("Division test");
-        Assert.assertEquals(1.9, 10 / 5.0, 0.2);// 0
+        Assertions.assertEquals(1.9, 10 / 5.0, 0.2);// 0
     }
 
     @Test
     public void external_resource_rule_boolean_test() {
         System.out.println("Boolean test");
         boolean variable = true;
-        Assert.assertTrue("variable is not true", variable);
-        Assert.assertFalse("!variable is not false", !variable);
+        Assertions.assertTrue(variable, "variable is not true");
+        Assertions.assertFalse(!variable, "!variable is not false");
     }
 }
