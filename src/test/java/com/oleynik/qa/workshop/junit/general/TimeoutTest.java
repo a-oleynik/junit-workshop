2d03c552fed6b8ada23140bf3794b5f678607747
Andrey Oleynik <5436433+a-oleynik@users.noreply.github.com>
2021-08-18 10:24:18 +0200

    fix for the TimeoutTest

--- src/test/java/com/oleynik/qa/workshop/junit/general/TimeoutTest.java
+++ src/test/java/com/oleynik/qa/workshop/junit/general/TimeoutTest.java
@@ -14,20 +14,20 @@ public class TimeoutTest {
     public void setUp() {
         Utils.waitFor(1);
         System.out.println("Set up");
-        System.out.println("Set up id " + Thread.currentThread().getId());
+        System.out.println("Set up thread id " + Thread.currentThread().getId());
     }
 
     @AfterEach
     public void tearDown() {
         Utils.waitFor(1);
         System.out.println("Tear down");
-        System.out.println("Tear down id " + Thread.currentThread().getId());
+        System.out.println("Tear down thread id " + Thread.currentThread().getId());
     }
 
     @Test
     public void timeout_test() {
         System.out.println("Timeout test");
-        System.out.println("Test id " + Thread.currentThread().getId());
+        System.out.println("Thread id " + Thread.currentThread().getId());
         assertTimeout(ofSeconds(6), () ->
                 Utils.waitFor(2)
         );
@@ -36,7 +36,7 @@ public class TimeoutTest {
     @Test
     public void timeout_test2() {
         System.out.println("Timeout test");
-        System.out.println("Test id " + Thread.currentThread().getId());
+        System.out.println("Thread id " + Thread.currentThread().getId());
         assertTimeout(ofSeconds(2), () ->
                 Utils.waitFor(1)//2
         );
@@ -46,7 +46,7 @@ public class TimeoutTest {
     @Test
     public void timeout_test3() {
         System.out.println("Timeout test");
-        System.out.println("Test id " + Thread.currentThread().getId());
+        System.out.println("Thread id " + Thread.currentThread().getId());
                 Utils.waitFor(1);//2
     }
 }
