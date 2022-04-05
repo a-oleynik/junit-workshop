1eb41ad05ae31ff7f3a1c83ba6c4e35f4bf09cae
Andrey Oleynik <5436433+a-oleynik@users.noreply.github.com>
2022-04-05 19:17:42 +0200

    PioneerCartesianProductTest was updated. Updated versions for opencsv, maven-compiler-plugin and maven-surefire-plugin

--- src/test/java/com/oleynik/qa/workshop/junit/ddt/PioneerCartesianProductTest.java
+++ src/test/java/com/oleynik/qa/workshop/junit/ddt/PioneerCartesianProductTest.java
@@ -1,17 +1,15 @@
 package com.oleynik.qa.workshop.junit.ddt;
 
 import com.oleynik.qa.workshop.junit.model.MyDoubleWrapper;
-import org.junitpioneer.jupiter.CartesianProductTest;
-import org.junitpioneer.jupiter.CartesianValueSource;
+import org.junitpioneer.jupiter.cartesian.CartesianTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PioneerCartesianProductTest {
 
-    @CartesianProductTest
-    @CartesianValueSource(ints = { 1, 2 })
-    @CartesianValueSource(ints = { 3, 4 })
-    void cartesian_product_test(int x, int y) {
+    @CartesianTest
+    void cartesian_product_test(@CartesianTest.Values(ints = {1, 2}) int x,
+                                @CartesianTest.Values(ints = {3, 4}) int y) {
         System.out.printf("Multiplying %s on %s%n", x, y);
         assertEquals(x * y, new MyDoubleWrapper(x).multiply(y), 0);
     }
