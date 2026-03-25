50d09b0fca91d5196b10660191c52dbbefdd87b3
Andrii Olieinik <5436433+a-oleynik@users.noreply.github.com>
2026-03-25 02:37:03 +0100

    feat: Add Before / After Suite emulation by JUnit extension

---
+++ src/test/java/com/oleynik/qa/workshop/junit/suite/extension/SuiteExtensionSecondTest.java
@@ -0,0 +1,13 @@
+package com.oleynik.qa.workshop.junit.suite.extension;
+
+import com.oleynik.qa.workshop.junit.extensions.SuiteLikeLifecycleExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SuiteLikeLifecycleExtension.class)
+public class SuiteExtensionSecondTest {
+    @Test
+    void second() {
+        System.out.println("extension case two");
+    }
+}
